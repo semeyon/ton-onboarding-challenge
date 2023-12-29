6a6ede97cbd341182499329a1a4062a3935ab4f6
Semeyon <semeyon@svetliy.ru>
2023-12-29 06:39:45 +0000

    Save more stuff

--- index.ts
+++ index.ts
@@ -4,6 +4,9 @@ import {Address, TonClient} from "ton"
 import {getHttpEndpoint} from "@orbs-network/ton-access";
 import {BN} from 'bn.js'
 
+import {unixNow} from "./src/lib/utils";
+import {MineMessageParams, Queries} from "./src/giver/NftGiver.data";
+
 
 async function main () {
 
@@ -37,6 +40,36 @@ async function main () {
   console.log('target_delta', target_delta.toString());
   console.log('min_cpl', min_cpl.toString());
   console.log('max_cpl', max_cpl.toString());
+
+  const mineParams : MineMessageParams = {
+    expire: unixNow() + 300, // 5 min is enough to make a transaction
+    mintTo: wallet, // your wallet
+    data1: new BN(0), // temp variable to increment in the miner
+    seed // unique seed from get_mining_data
+  };
+  
+  let msg = Queries.mine(mineParams);
+
+  let progress = 0;
+
+  while (new BN(msg.hash(), 'be').gt(complexity)) {
+    progress += 1
+    console.clear()
+    console.log(`Mining started: please, wait for 30-60 seconds to mine your NFT!`)
+    console.log(' ')
+    console.log(`⛏ Mined ${progress} hashes! Last: `, new BN(msg.hash(), 'be').toString())
+
+    mineParams.expire = unixNow() + 300
+    mineParams.data1.iaddn(1)
+    msg = Queries.mine(mineParams)
+  }
+
+  console.log(' ')
+  console.log('💎 Mission completed: msg_hash less than pow_complexity found!');
+  console.log(' ')
+  console.log('msg_hash: ', new BN(msg.hash(), 'be').toString())
+  console.log('pow_complexity: ', complexity.toString())
+  console.log('msg_hash < pow_complexity: ', new BN(msg.hash(), 'be').lt(complexity))
 
 }
 
